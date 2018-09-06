98f66a6821e5fbaee9257dbc27c798088fe85ec7
Mr Blunt <carl.adams97@gmail.com>
2018-09-06 13:25:25 -0500

    added code to write to files for PyPoll which is 98% complete now. PyBank, still need to add date pointing code for Max and Min values as well as writing to a file.

--- PyBank/Python/main.py
+++ PyBank/Python/main.py
@@ -4,12 +4,13 @@ import pandas as pd
 # define and initiate some variables for counting months and financial data
 i = 0
 month_count = 0
-monthly_p_l_list = []
-monthly_p_l_list_shift = [0]
+date_list = []
+profit_l_list = []
+profit_l_list_shift = []
 total_p_l = 0
-p_l_difference = float(0)
-p_l_difference_sum = float(0)
-p_l_difference_list = []
+profit_l_difference = float(0)
+profit_l_difference_list = []
+i = 0
 
 # define the path to the file that contains the results of the election
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
@@ -19,32 +20,47 @@ with open(csvpath, 'r', newline='') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
 # define the header of the csv file just opened and read and skip it when analysis begins
     csv_header = next(csv_reader)
-# loop through the dataset to create list of p_l values
+# loop through the dataset to count months and add to list of dates and profit/loss values
     for row in csv_reader:
         month_count += 1
-        monthly_p_l_list.append(float(row[1]))
+        profit_l_list.append(float(row[1]))
+        profit_l_list_shift.append(float(row[1]))
+        date_list.append(str(row[0]))
+
+# Build profit/loss difference list
+for p_l_value in profit_l_list and profit_l_list_shift:
+        profit_l_difference = profit_l_list[1] - profit_l_list_shift[0]
+
+        profit_l_difference_list.append(profit_l_difference)
         
-        # monthly_difference = monthly_income(i+1) - monthly_income(i)
-        # monthly_difference_list.append(monthly_difference[i])
-        # i += 1
+
+# define function to compute average change in profit/loss between months
+
+def average(profit_l_difference_list):
+    x = len(profit_l_difference_list)
+    total = sum(profit_l_difference_list)
+    avg = total / x
+    return avg
+
+profit_l_change = average(profit_l_difference_list)
+# print report    
 print("Financial Analysis")
 print("------------------------------")
 print(f"Total Months: {month_count}")
-total_p_l = sum(monthly_p_l_list)
+total_p_l = sum(profit_l_list)
 print(f"Total: ${total_p_l}")
-highest_p_l = max(monthly_p_l_list)
+print(f"Average Change: ${profit_l_change}")
+
+# to match the dates with the highest and lowest profit/loss values, we zip the lists together
+highest_p_l = max(profit_l_list) 
+lowest_p_l = min(profit_l_list)
+
 print(f"Greatest Increase in Profits: [date] ({highest_p_l})")
-lowest_p_l = min(monthly_p_l_list)
+
 print(f"Greatest Decrease in Profits: [date] ({lowest_p_l})")
 
 
 
-# make a list of differences between monthly p/l values
-
-# for p_l in monthly_p_l_list:
-   # p_l_difference = p_l[i + 1] - p_l[i]
-  #  p_l_difference_list.append(p_l_difference)
-# print(p_l_difference_list)
 
 
 
